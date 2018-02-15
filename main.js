805eda58405792b31f004c3244bef014f3fdbe96
Karolina Kadela <karolina.kadela03@gmail.com>
2018-02-15 10:11:02 +0100

    cv

---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+    var toggleMenu = function() {
+        $('.bar1').toggleClass('change-bar1');
+        $('.bar2').toggleClass('change-bar2');
+        $('.bar3').toggleClass('change-bar3');
+        $('.side-bar').toggleClass('side-bar-expand', function() {
+            $('.side-bar-menu').toggleClass('side-bar-menu-expand');
+            $('.sbm-li').fadeToggle(800);
+        });
+        if ( $(document).width() >= 1024 ) {
+            $('.main-content').toggleClass('scooched');
+        } else {
+            $('.main-content').toggleClass('just-opa')
+        }
+    };
+
+    var hideMenu = function() {
+        $('.bar1').removeClass('change-bar1');
+        $('.bar2').removeClass('change-bar2');
+        $('.bar3').removeClass('change-bar3');
+        $('.side-bar').removeClass('side-bar-expand', function() {
+            $('.side-bar-menu').removeClass('side-bar-menu-expand');
+            $('.sbm-li').fadeOut(800);
+        });
+        if ( $(document).width() >= 1024 ) {
+            $('.main-content').removeClass('scooched');
+        } else {
+            $('.main-content').removeClass('just-opa')
+        }
+    };
+  
+    $('#open-nav').on('click', toggleMenu);
+
+    $('.main-content').click(function (e) {
+        if (e.target != $('.side-bar-expand')) {
+            hideMenu();
+        }
+    });
+
+    $('.side-bar a').on('click', function() {
+        hideMenu();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 1000);
+        return false;
+    });
+
+    $('.icon-wrapper').hover(function() {
+        $(this).toggleClass('icon-wrapper-extend');
+    });
+
+});
